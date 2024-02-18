f9548e664d6dd00a663d62ee67bbb5ee4176bf4b
Babangida Tsowa <tsowababangida@gmail.com>
2024-02-18 19:44:23 +0100

    task(ur-4): added gulp-imagemin for image optimization in build prcess

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
 
 gulp.task("default", () => {
   console.log("Hello Gulp!");
+});
+
+gulp.task("imagemin", () => {
+  return gulp
+    .src("public/images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("public/images/minified"));
 });
