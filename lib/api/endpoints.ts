14ac04043cc1385fde7783853e6d1313148908af
Babangida Tsowa <tsowababangida@gmail.com>
2024-05-10 09:09:31 +0100

    Documented ReactQuery Updates

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -1,3 +1,12 @@
+/**
+ * Enum representing different API endpoints.
+ * @enum {string} ENDPOINT
+ */
 enum ENDPOINT {
+    /**
+     * Represents the login endpoint.
+     * @type {string}
+     * @memberof ENDPOINT
+     */
     "LOGIN" = "login"
 }
--- lib/api/request.ts
+++ lib/api/request.ts
@@ -1,6 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetch } from '../api';
 
+/**
+ * Represents the request details for a server request.
+ *
+ * @interface IQueryRequest
+ * @property {string | string[]} key - A unique identifier for the request.
+ * @property {'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'} method - The HTTP method to use for the request.
+ * @property {string} url - The URL to send the request to.
+ * @property {any} [params] - Data to be sent to the server.
+ */
 interface IQueryRequest {
     key: string | string[];
     method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
@@ -8,15 +17,27 @@ interface IQueryRequest {
     params?: any; // data to be sent to the server
 }
 
-const request = ({ key, method, url, params }: IQueryRequest) => {
+/**
+ * Makes a request to the server using the specified method and URL.
+ *
+ * @param {IQueryRequest} options - An object containing the request details.
+ * @param {string | string[]} options.key - A unique identifier for the request.
+ * @param {'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'} options.method - The HTTP method to use for the request.
+ * @param {string} options.url - The URL to send the request to.
+ * @param {any} [options.params] - Data to be sent to the server.
+ *
+ * @returns {Promise<{ data: any, isError: boolean, error: any, isLoading: boolean }>} 
+ *  - A promise that resolves to an object containing the request data, error, loading status, and whether an error occurred.
+ */
+const request = async ({ key, method, url, params }: IQueryRequest) => {
     const { data, isError, error, isLoading } = useQuery({
         queryKey: [key],
         queryFn: () => {
-            fetch({
+            return fetch({
                 method,
                 url,
                 data: params,
-            })
+            });
         }
     });
 
